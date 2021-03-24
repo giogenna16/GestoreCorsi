4e2281cc972c433a85e26c6a3514a6bacbfb5747
genna <genna@LAPTOP-HDR1FFT9.station>
2021-03-24 15:05:30 +0100

    Lezione 24/03

--- src/main/java/it/polito/tdp/corsi/model/Model.java
+++ src/main/java/it/polito/tdp/corsi/model/Model.java
@@ -1,7 +1,26 @@
 package it.polito.tdp.corsi.model;
 
+import java.util.List;
+import java.util.Map;
+
+import it.polito.tdp.corsi.db.CorsoDAO;
 
 public class Model {
 	
+	private  CorsoDAO corsoDAO;
+	
+	public Model() {
+		this.corsoDAO= new CorsoDAO();
+	}
+	
+	public List<Corso> getCorsiByPeriodo(Integer periodo){
+		return corsoDAO.getCorsiByPeriodo(periodo);
+		
+	}
+	
+	public Map<Corso, Integer> getIscrittiByPeriodo(Integer periodo){
+		
+		return corsoDAO.getIscrittiByPeriodo(periodo);
+	}
 	
 }
